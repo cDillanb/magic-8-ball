1f6a2f45c7006b44973b4c34404590328d1f5c82
cDillanb <85910223+cDillanb@users.noreply.github.com>
2022-02-02 11:14:04 -0700

    finished challenge and updated readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.pink,
+          appBar: AppBar(
+            backgroundColor: Colors.pinkAccent,
+            title: Text('Magic 8'),
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNum = 1;
+
+  void ballPress() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset('images/ball$ballNum.png'),
+        onPressed: () {
+          ballPress();
+        },
+      ),
+    );
+  }
+}
